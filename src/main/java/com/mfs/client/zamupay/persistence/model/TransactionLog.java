de3d5939390b1d2f9bb8040beaf7e070aabbd3f6
Tebogo Assagaai <tebogo.assagaai@xgileit.com>
2022-06-08 12:33:59 +0200

    refactored implementation

--- src/main/java/com/mfs/client/zamupay/persistence/model/TransactionLog.java
+++ src/main/java/com/mfs/client/zamupay/persistence/model/TransactionLog.java
@@ -16,7 +16,7 @@ import java.util.Date;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "bank_transaction_log")
+@Table(name = "transaction_log")
 public class TransactionLog {
 
     @Id
@@ -24,26 +24,17 @@ public class TransactionLog {
     @Column(name = "transaction_log_id", nullable = false)
     private int transactionLogId;
 
-    @Column(name = "transaction_id")
-    private String transactionId;
-
     @Column(name = "mfs_reference_id", nullable = false)
     private String mfsReferenceId;
-    
-    @Column(name = "receiver_msisdn", nullable = false)
-    private String receiverMsisdn;
 
-    @Column(name = "primary_account_number", nullable = false)
-    private String primaryAccountNumber;
+    @Column(name = "payment_notes", nullable = false)
+    private String paymentNotes;
+
+    @Column(name = "recipient_phone_number", nullable = false)
+    private String recipientPhoneNumber;
 
     @Column(name = "amount", nullable = false)
     private Double amount;
-
-    @Column(name = "result_code")
-    private int resultCode;
-
-    @Column(name = "result_desc")
-    private String resultDesc;
 
     @Column(name = "dateLogged", nullable = false)
     private Date dateLogged;
